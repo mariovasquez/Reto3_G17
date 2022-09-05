1e93dfb41c457f29468d932f0204b2157fb68920
mariovasquez <N00156666@upn.pe>
2022-09-05 06:09:02 -0500

    Update index.html about.html sass js

--- pages/about/js/modules/header.js
+++ pages/about/js/modules/header.js
@@ -1,5 +1,4 @@
 const header = () => {
-    const header = document.querySelector('.header');
     const nav = document.querySelector('.nav');
     const menuLinks = document.querySelectorAll('.menu__link');
     const navActionMenu = document.getElementById('navActionMenu');
@@ -13,7 +12,6 @@ const header = () => {
       nav.classList.remove('nav--open');
     };
     
-    document.addEventListener('scroll', () => {header.classList.toggle('header--scroll', scrollY > 0)});
     navActionMenu.addEventListener('click', openMenu);
     navActionClose.addEventListener('click', closeMenu);
     for (const menuLink of menuLinks) menuLink.addEventListener('click', closeMenu);
