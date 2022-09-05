546869470f26f9dfc5c804098040c060b12ba96b
mariovasquez <N00156666@upn.pe>
2022-09-05 01:52:11 -0500

    Update counter

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 'use strict';
-
+import counter from "./modules/counter.js";
 import header from "./modules/header.js";
 import switcher from "./modules/switcher.js";
 
 const documentReady = () => {
-  
+  counter();
   header();
   switcher();
 };
