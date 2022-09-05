3271bb3a7b884ed45e115344057b4eefa40025a4
mariovasquez <N00156666@upn.pe>
2022-09-05 18:17:21 -0500

    Update featured section

--- js/modules/featured.js
+++ js/modules/featured.js
@@ -1,8 +1,40 @@
 const featured = () => {
     const newReleases = document.getElementById('newReleases');
     const popular = document.getElementById('popular');
-    const newRealesAlbums = document.getElementById('newReleasesAlbums');
+    const newReleasesAlbums = document.getElementById('newReleasesAlbums');
     const popularAlbums = document.getElementById('popularAlbums');
+    const featuredAlbumContainers = document.querySelectorAll('.section__featured-album-container');
+    const buttonsBuy = document.querySelectorAll('.button-text--buy');
+    const albumImages = document.querySelectorAll('.section__featured-album-img');
+
+    console.log(featuredAlbumContainers);
+
+    for (const featuredAlbumContainer of featuredAlbumContainers) {
+        featuredAlbumContainer.addEventListener('mouseover', () => {
+            for (const buttonBuy of buttonsBuy) {
+                if(featuredAlbumContainer.id === buttonBuy.id){
+                    buttonBuy.classList.add('button-text--buy--enable');
+                }
+            }
+            for (const albumImage of albumImages) {
+                if(featuredAlbumContainer.id === albumImage.id){
+                    albumImage.classList.add('section__featured-album-img--hover');
+                }
+            }
+        });
+        featuredAlbumContainer.addEventListener('mouseout', () => {
+            for (const buttonBuy of buttonsBuy) {
+                if(featuredAlbumContainer.id === buttonBuy.id){
+                    buttonBuy.classList.remove('button-text--buy--enable');
+                }
+            }
+            for (const albumImage of albumImages) {
+                if(featuredAlbumContainer.id === albumImage.id){
+                    albumImage.classList.remove('section__featured-album-img--hover');
+                }
+            }
+        });
+    }
 
     
     newReleases.addEventListener('click', () => {
